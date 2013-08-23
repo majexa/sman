6e8e7c3b0f818edf72f08606d5975517339585a6
droplet-installer <masted311@gmail.com>
2013-08-23 13:39:37 +0000

    ...

---
+++ lib/DigitaloceanApiCached.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @method void api($uri, $data)
+ * @method void createServer($name, $opts)
+ * @method void servers()
+ * @method void _destroyServer($id)
+ * @method void destroyDroplet($name)
+ */
+class DigitaloceanApiCached extends ObjectCacher {
+
+  protected function getObject() {
+    return new DigitaloceanApi;
+  }
+
+  function cachedMethods() {
+    return [
+      'servers'
+    ];
+  }
+
+}
