8e6f111e883340af63ea2974e6d55a25bbe2e711
masted <masted311@gmail.com>
2014-07-22 17:19:04 +0400

    Commit was made from server localhost by ci/push

--- lib/TestWeb.class.php
+++ lib/TestWeb.class.php
@@ -5,7 +5,7 @@ class TestWeb extends NgnTestCase {
   function test() {
     $server = require dirname(SMAN_PATH).'/config/server.php';
     $url = 'default.'.$server['baseDomain'];
-    $this->assertTrue((bool)strstr(`wget -qO- $url`, 'NGN: setup success'));
+    $this->assertTrue((bool)strstr(`wget -qO- $url`, 'default ngn host'));
   }
 
 }
